945a55eb9671a46842bd7a04b02e3c1f70f7b535
Iver Egge <iverasp@gmail.com>
2024-09-20 15:06:39 +0200

    Change slack webhook

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_cloudwatch_metric_alarm" "dead_letter_queue_alarm" {
   period              = 60
   threshold           = var.threshold
   treat_missing_data  = "notBreaching"
-  alarm_description   = "${upper(var.environment)}: ${var.queue_name} SQS message(s) visible"
+  alarm_description   = "${var.queue_name} SQS message(s) visible"
   actions_enabled     = true
   alarm_actions       = [aws_sns_topic.sns_alarm_topic.arn]
   ok_actions          = [aws_sns_topic.sns_alarm_topic.arn]
@@ -18,8 +18,8 @@ resource "aws_cloudwatch_metric_alarm" "dead_letter_queue_alarm" {
   }
 }
 
-data "aws_ssm_parameter" "personnel_slack_hook_url" {
-  name      = "/config/slack/personell-slack-hook-url"
+data "aws_ssm_parameter" "slack_webhook_url" {
+  name = "/config/slack/webhooks/alerts"
 }
 
 resource "aws_sns_topic" "sns_alarm_topic" {
@@ -30,7 +30,7 @@ module "alarms_to_slack" {
   source      = "github.com/nsbno/terraform-aws-alarms-to-slack?ref=3b8197b"
   name_prefix = var.name_prefix
   slack_webhook_urls = [
-    data.aws_ssm_parameter.personnel_slack_hook_url.value,
+    data.aws_ssm_parameter.slack_webhook_url.value,
   ]
   sns_topic_arns = [
     aws_sns_topic.sns_alarm_topic.arn
